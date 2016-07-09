486a5d534ecf995eb89d16b20f498a6ed12669a7
dearHaoGeGe <862157904@qq.com>
2016-07-10 00:24:42 +0800

    添加了几个网址

--- app/src/main/java/com/my/simplesampletest/segment_view/SegmentViewActivity.java
+++ app/src/main/java/com/my/simplesampletest/segment_view/SegmentViewActivity.java
@@ -8,6 +8,12 @@ import com.my.simplesampletest.base.BaseActivity;
 /**
  * RadioGroup实现类似ios的分段选择(UISegmentedControl)控件
  *
+ * http://www.jcodecraeer.com/a/anzhuokaifa/androidkaifa/2014/0512/1615.html
+ * https://github.com/Kaopiz/android-segmented-control
+ *
+ * Android开发的精华包括工具、中文API
+ * http://www.androiddevtools.cn/
+ *
  * Created by YJH on 2016/7/9.
  */
 public class SegmentViewActivity extends BaseActivity {
