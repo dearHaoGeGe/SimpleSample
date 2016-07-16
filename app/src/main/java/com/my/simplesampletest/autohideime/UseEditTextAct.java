3e032b1ffb255013c7cd0c8bc83b5a56a4b23514
dearHaoGeGe <862157904@qq.com>
2016-07-16 15:49:10 +0800

    合并代码

--- app/src/main/java/com/my/simplesampletest/autohideime/UseEditTextAct.java
+++ app/src/main/java/com/my/simplesampletest/autohideime/UseEditTextAct.java
@@ -22,12 +22,13 @@ import com.orhanobut.logger.Logger;
  */
 public class UseEditTextAct extends BaseActivity implements View.OnClickListener, TextWatcher, View.OnKeyListener {
 
-    private static final String TAG="EditText需要注意的地方";
-    private EditText ed_zh_cn,ed_en;                //设置默认输入法
-    private EditText ed_open_input,ed_close_input;  //打开和关闭输入法
+    private static final String TAG = "EditText需要注意的地方";
+    private EditText ed_zh_cn, ed_en;                //设置默认输入法
+    private EditText ed_open_input, ed_close_input;  //打开和关闭输入法
     private EditText ed_addTextChangedListener;     //监听EditText的输入状态
     private EditText ed_OnKeyListener;              //监听输入法中的回车按钮
-    private TextView tv_open_input,tv_close_input;
+    private EditText ed_InputTxtFilter;     //限制输入内容
+    private TextView tv_open_input, tv_close_input;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,15 +41,16 @@ public class UseEditTextAct extends BaseActivity implements View.OnClickListener
 
     @Override
     public void initView() {
-        ed_zh_cn= (EditText) findViewById(R.id.ed_zh_cn);
-        ed_en= (EditText) findViewById(R.id.ed_en);
+        ed_zh_cn = (EditText) findViewById(R.id.ed_zh_cn);
+        ed_en = (EditText) findViewById(R.id.ed_en);
 
-        ed_open_input= (EditText) findViewById(R.id.ed_open_input);
-        ed_close_input= (EditText) findViewById(R.id.ed_close_input);
-        tv_open_input= (TextView) findViewById(R.id.tv_open_input);
-        tv_close_input= (TextView) findViewById(R.id.tv_close_input);
-        ed_addTextChangedListener= (EditText) findViewById(R.id.ed_addTextChangedListener);
-        ed_OnKeyListener= (EditText) findViewById(R.id.ed_OnKeyListener);
+        ed_open_input = (EditText) findViewById(R.id.ed_open_input);
+        ed_close_input = (EditText) findViewById(R.id.ed_close_input);
+        tv_open_input = (TextView) findViewById(R.id.tv_open_input);
+        tv_close_input = (TextView) findViewById(R.id.tv_close_input);
+        ed_addTextChangedListener = (EditText) findViewById(R.id.ed_addTextChangedListener);
+        ed_OnKeyListener = (EditText) findViewById(R.id.ed_OnKeyListener);
+        ed_InputTxtFilter= (EditText) findViewById(R.id.ed_InputTxtFilter);
 
         tv_open_input.setOnClickListener(this);
         tv_close_input.setOnClickListener(this);
@@ -74,19 +76,21 @@ public class UseEditTextAct extends BaseActivity implements View.OnClickListener
          * IME_ACTION_DONE 完成
          */
         ed_OnKeyListener.setImeOptions(EditorInfo.IME_ACTION_SEARCH);
+
+        InputTxtFilter.inputFilter(this,ed_InputTxtFilter,InputTxtFilter.INPUT_TYPE_EN,5);
     }
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.tv_open_input:
                 Toast.makeText(UseEditTextAct.this, "打开输入法~", Toast.LENGTH_SHORT).show();
-                openInput(this,ed_open_input);
+                openInput(this, ed_open_input);
                 break;
 
             case R.id.tv_close_input:
                 Toast.makeText(UseEditTextAct.this, "关闭输入法~", Toast.LENGTH_SHORT).show();
-                closeInput(this,ed_close_input);
+                closeInput(this, ed_close_input);
                 break;
         }
     }
@@ -98,7 +102,7 @@ public class UseEditTextAct extends BaseActivity implements View.OnClickListener
      * @param context
      * @param editText
      */
-    private void openInput(Context context, View editText){
+    private void openInput(Context context, View editText) {
         InputMethodManager inputMethodManager = (InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE);
         inputMethodManager.showSoftInput(editText, 0);
     }
@@ -109,7 +113,7 @@ public class UseEditTextAct extends BaseActivity implements View.OnClickListener
      * @param context
      * @param editText
      */
-    private void closeInput(Context context, View editText){
+    private void closeInput(Context context, View editText) {
         InputMethodManager inputMethodManager = (InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE);
         inputMethodManager.hideSoftInputFromWindow(editText.getWindowToken(), 0);
     }
@@ -140,7 +144,7 @@ public class UseEditTextAct extends BaseActivity implements View.OnClickListener
      */
     @Override
     public void afterTextChanged(Editable s) {
-        Logger.d(TAG,s.toString());
+        Logger.d(TAG, s.toString());
     }
     //********************************addTextChangedListener  结束*********************************//
 
@@ -155,7 +159,7 @@ public class UseEditTextAct extends BaseActivity implements View.OnClickListener
      */
     @Override
     public boolean onKey(View v, int keyCode, KeyEvent event) {
-        if (keyCode==KeyEvent.KEYCODE_ENTER && event.getAction()==KeyEvent.ACTION_UP){
+        if (keyCode == KeyEvent.KEYCODE_ENTER && event.getAction() == KeyEvent.ACTION_UP) {
             Toast.makeText(UseEditTextAct.this, "手指弹起时执行确认功能", Toast.LENGTH_SHORT).show();
             return true;
         }
