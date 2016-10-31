2a62badb3307a51cd6bbfc04dbdd5cca54a8a69d
dearHaoGeGe <862157904@qq.com>
2016-10-31 14:49:12 +0800

    添加了几个工具类

--- app/src/main/java/com/my/simplesampletest/utils/PhoneStateUtils.java
+++ app/src/main/java/com/my/simplesampletest/utils/PhoneStateUtils.java
@@ -8,6 +8,7 @@ import java.text.DecimalFormat;
 import android.annotation.TargetApi;
 import android.app.Activity;
 import android.app.ActivityManager;
+import android.app.KeyguardManager;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
@@ -50,8 +51,77 @@ public class PhoneStateUtils {
             }   
         }   
         return false;   
-    }  
-	
+    }
+
+	/**
+	 * 检测网络是否可用
+	 *
+	 * @return true可用   false不可用
+	 */
+	public static boolean isNetworkConnected(Context context) {
+		ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+		NetworkInfo ni = cm.getActiveNetworkInfo();
+		return ni != null && ni.isConnectedOrConnecting();
+	}
+
+	/**
+	 * 获取当前网络类型
+	 *
+	 * @return 0：没有网络   1：WIFI网络   2：WAP网络    3：NET网络
+	 */
+	public static int getNetworkType(Context context) {
+		int NETTYPE_WIFI = 0x01;
+		int NETTYPE_CMWAP = 0x02;
+		int NETTYPE_CMNET = 0x03;
+		int netType = 0;
+		ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+		NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+		if (networkInfo == null) {
+			return netType;
+		}
+		int nType = networkInfo.getType();
+		if (nType == ConnectivityManager.TYPE_MOBILE) {
+			String extraInfo = networkInfo.getExtraInfo();
+//            if(!StringUtils.isEmpty(extraInfo)){
+			if (!(extraInfo == null || extraInfo.trim().length() == 0)) {
+				if (extraInfo.toLowerCase().equals("cmnet")) {
+					netType = NETTYPE_CMNET;
+				} else {
+					netType = NETTYPE_CMWAP;
+				}
+			}
+		} else if (nType == ConnectivityManager.TYPE_WIFI) {
+			netType = NETTYPE_WIFI;
+		}
+		return netType;
+	}
+
+	/**
+	 * 判断是否为锁屏状态
+	 *
+	 * @return true 锁屏      false 没锁屏
+	 */
+	private static boolean isLockScreen(Context context) {
+		KeyguardManager km = (KeyguardManager) context.getSystemService(Context.KEYGUARD_SERVICE);
+		if (km.inKeyguardRestrictedInputMode()) {
+			//处于锁屏状态
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	/**
+	 * 获取当前的手机号
+	 *
+	 * @return 手机号
+	 */
+	public static String getLocalNumber(Context context) {
+		TelephonyManager tManager = (TelephonyManager) context.getSystemService(context.TELEPHONY_SERVICE);
+		String number = tManager.getLine1Number();
+		return number;
+	}
+
 	/**
 	 * 判断是否为2或者3G
 	 * @return
