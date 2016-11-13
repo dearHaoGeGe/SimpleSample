d2561fa57baff88f46d546032e39c5216700a75b
dearHaoGeGe <862157904@qq.com>
2016-11-13 23:13:34 +0800

    图片添加水印方法

--- app/src/main/java/com/my/simplesampletest/pic_save_sd/SaveAndReadPic.java
+++ app/src/main/java/com/my/simplesampletest/pic_save_sd/SaveAndReadPic.java
@@ -2,8 +2,13 @@ package com.my.simplesampletest.pic_save_sd;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.Typeface;
 import android.os.Environment;
+import android.text.TextPaint;
 import android.util.Log;
 
 import java.io.ByteArrayInputStream;
@@ -197,10 +202,60 @@ public class SaveAndReadPic {
      * @param fileName 图片的名字
      * @return 整个的路径名字
      */
-    public static String mergePicName(String fileName) {
+    private static String mergePicName(String fileName) {
         return outPath + "/" + fileName + ".jpg";
     }
 
+
+    /**
+     * 为图片在右下角加水印
+     *
+     * @param src       需要加水印的图片
+     * @param watermark 水印 Bitmap
+     * @param title     文字
+     * @return Bitmap
+     */
+    public static Bitmap watermarkBitmap(Bitmap src, Bitmap watermark,
+                                         String title) {
+        if (src == null) {
+            return null;
+        }
+        int w = src.getWidth();
+        int h = src.getHeight();
+        // 需要处理图片太大造成的内存超过的问题,这里我的图片很小所以不写相应代码了
+        Bitmap newb = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);// 创建一个新的和SRC长度宽度一样的位图
+        Canvas cv = new Canvas(newb);
+        cv.drawBitmap(src, 0, 0, null);// 在 0，0坐标开始画入src
+        Paint paint = new Paint();
+        // 加入图片
+        if (watermark != null) {
+            int ww = watermark.getWidth();
+            int wh = watermark.getHeight();
+            paint.setAlpha(50);
+            cv.drawBitmap(watermark, w - ww + 5, h - wh + 5, paint);// 在src的右下角画入水印
+        }
+        // 加入文字
+        if (title != null) {
+            String familyName = "宋体";
+            Typeface font = Typeface.create(familyName, Typeface.BOLD);
+            TextPaint textPaint = new TextPaint();
+            textPaint.setColor(Color.WHITE);
+            textPaint.setTypeface(font);
+            textPaint.setTextSize(22);
+
+            float len = textPaint.measureText(title);
+            // 这里是自动换行的
+            // StaticLayout layout = new
+            // StaticLayout(title,textPaint,w,Alignment.ALIGN_NORMAL,1.0F,0.0F,true);
+            // layout.draw(cv);
+            cv.drawText(title, w - len - 5, h - 20, textPaint);// 在src的右下角写上文字
+            // 文字就加左上角算了
+            // cv.drawText(title,0,40,paint);
+        }
+        cv.save(Canvas.ALL_SAVE_FLAG);// 保存
+        cv.restore();// 存储
+        return newb;
+    }
 
 //    File file = new File(outPath);
 //    if (file.canExecute()) {
