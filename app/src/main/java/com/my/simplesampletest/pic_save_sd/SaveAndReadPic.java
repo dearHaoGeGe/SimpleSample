818c60b08abb2a8385a3dbac26b89e6db3b59839
dearHaoGeGe <862157904@qq.com>
2016-11-27 16:53:53 +0800

    设置图片为黑白色

--- app/src/main/java/com/my/simplesampletest/pic_save_sd/SaveAndReadPic.java
+++ app/src/main/java/com/my/simplesampletest/pic_save_sd/SaveAndReadPic.java
@@ -7,6 +7,7 @@ import android.graphics.Color;
 import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.Typeface;
+import android.media.ThumbnailUtils;
 import android.os.Environment;
 import android.text.TextPaint;
 import android.util.Log;
@@ -257,14 +258,56 @@ public class SaveAndReadPic {
         return newb;
     }
 
-//    File file = new File(outPath);
-//    if (file.canExecute()) {
-//        Log.v(TAG, "可执行文件");
-//    }
-//    if (file.canRead()) {
-//        Log.v(TAG, "可读");
-//    }
-//    if (file.canWrite()) {
-//        Log.v(TAG, "可写");
-//    }
+
+    /**
+     *  File file = new File(outPath);
+     *      if (file.canExecute()) {
+     *          Log.v(TAG, "可执行文件");
+     *      }
+     *      if (file.canRead()) {
+     *          Log.v(TAG, "可读");
+     *      }
+     *      if (file.canWrite()) {
+     *          Log.v(TAG, "可写");
+     *      }
+     */
+
+
+    /**
+     * 将彩色图转换为纯黑白二色图片
+     *
+     * @param bmp 位图
+     * @return 返回转换好的位图
+     */
+    public static Bitmap convertToBlackWhite(Bitmap bmp) {
+        int width = bmp.getWidth(); // 获取位图的宽
+        int height = bmp.getHeight(); // 获取位图的高
+        int[] pixels = new int[width * height]; // 通过位图的大小创建像素点数组
+
+        bmp.getPixels(pixels, 0, width, 0, 0, width, height);
+        int alpha = 0xFF << 24;
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                int grey = pixels[width * i + j];
+
+                //分离三原色
+                int red = ((grey & 0x00FF0000) >> 16);
+                int green = ((grey & 0x0000FF00) >> 8);
+                int blue = (grey & 0x000000FF);
+
+                //转化成灰度像素
+                grey = (int) (red * 0.3 + green * 0.59 + blue * 0.11);
+                grey = alpha | (grey << 16) | (grey << 8) | grey;
+                pixels[width * i + j] = grey;
+            }
+        }
+        //新建图片
+        Bitmap newBmp = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565);
+        //设置图片数据
+        newBmp.setPixels(pixels, 0, width, 0, 0, width, height);
+
+        Bitmap resizeBmp = ThumbnailUtils.extractThumbnail(newBmp, 380, 460);
+        return resizeBmp;
+    }
+
 }
